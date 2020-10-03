db31c2bf0ddcb9aaeee074a4e52bef5dd7938fb2
bradengibson <32143413+bradengibson@users.noreply.github.com>
2020-10-03 15:07:05 -0400

    Added WarframeItemsJson files and objects for deserializing Warframes

---
+++ WarframeToDo/Objects/Items/WarframeComponent.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WarframeToDo.Objects.Items
+{
+    public class WarframeComponent
+    {
+        [JsonProperty("uniqueName")]
+        public string UniqueName { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("itemCount")]
+        public Nullable<int> ItemCount { get; set; }
+
+        [JsonProperty("imageName")]
+        public string ImageName { get; set; }
+
+        [JsonProperty("tradable")]
+        public Nullable<Boolean> Tradable { get; set; }
+
+        [JsonProperty("primeSellingPrice")]
+        public Nullable<int> PrimeSellingPrice { get; set; }
+
+        [JsonProperty("ducats")]
+        public Nullable<int> Ducats { get; set; }
+
+        [JsonProperty("drops")]
+        public List<Drop> Drops { get; set; }
+
+        public WarframeComponent()
+        {
+            Drops = new List<Drop>();
+        }
+    }
+}
